9f1642b44b3c7f11aea0a099e6540d0f5914981b
Patrick Marck D <patrickmarckdulaca@gmail.com>
2026-08-11 22:06:36 +0800

    update

--- src/environments/environment.web-dev.ts
+++ src/environments/environment.web-dev.ts
@@ -1,10 +1,10 @@
 export const environment = {
   platform: 'web-dev',
   env: 'web',
-  SERVER_URL_MAIN: 'http://192.168.1.17:4324/api',
-  SERVER_URL_CLUSTERS: 'http://192.168.1.17:4324/api',
-  WEBSOCKET_MAIN: 'ws://192.168.1.17:4324/',
- 
+  SERVER_URL_MAIN: 'http://localhost:4324/api',
+  SERVER_URL_CLUSTERS: 'http://localhost:4324/api',
+  WEBSOCKET_MAIN: 'ws://localhost:4324/',
+
   MAPTILER: {
     KEY: 'pUXHYccQKUwJOa79LJMh',
   },
